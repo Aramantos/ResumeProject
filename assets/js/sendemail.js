6a421fa4098b3e7bcc9e48335600c51f3ae3beae
Aramantos <john.doyle.mail@icloud.com>
2020-12-05 15:00:20 +0000

    Implemented emailjs intigration on contact page

---
+++ assets/js/sendemail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("iCloud_Email_JS", "project_form", { 
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error)
+        });
+        return false;
+}
